a7b1e080bba2b1efafdd0da33bc06f4f1a1c6a9b
Odon_ML <odon@integriapp.com>
2021-07-31 15:30:34 -0500

    final section 4 - modules / routers

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  exports: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }

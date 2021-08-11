4db206ba6b0159642b7cfe50e65b9a196f7e7822
Odon_ML <odon@integriapp.com>
2021-08-10 21:13:13 -0500

    end of section 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +19,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   imports: [
     CommonModule,
